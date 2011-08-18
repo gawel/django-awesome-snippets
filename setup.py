75af9901ae995cdd52bab01667bf039629fb3f55
Gael Pasgrimaud <gael@gawel.org>
2011-08-18 22:14:43 +0200

    0.2

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.1'
+version = '0.2'
 
 setup(name='django-awesome-snippets',
       version=version,
-      description="",
+      description="A decorator to generate and cache html snippets",
       long_description=open('README.txt').read(),
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='',
